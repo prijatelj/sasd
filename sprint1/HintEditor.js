db86846f6af8697a6125d873c09176db1763391a
prijatelj <theomnipotentwoof@gmail.com>
2017-03-06 23:03:26 -0500

    included simple Upload Image button

--- sprint1/HintEditor.js
+++ sprint1/HintEditor.js
@@ -6,27 +6,6 @@
  */
 
 /**
- * applies the EventListener to the element with the id "hintSwitch". When
- * hintSwitch is activated, then it will generate the hintEditor
- *
- * FIXME currently not used, due to onclick attribute of button, being less
- * code to write in the html. So this is unused and may be removed.
- */
-function hintSwitchListener(){
-    var hintSwitch = document.getElementById("hintSwitch");
-
-    hintSwitch.addEventListener("click", toggleHintEditor, false);
-
-    /* Probably the proper way to add event listeners. Caters to IE browsers.
-    if (hintSwitch.addEventListener){
-        hintSwitch.addEventListener("click", toggleHintEditor(), false);
-    } else if (hintSwitch.attachEvent) {
-        hintSwitch.attachEvent('onclick', toggleHintEditor());
-    }
-    */
-}
-
-/**
  * toggles the hint editor. Each hint's individual content may be edited by
  * selecting the proper hint number in the drop down. select add hint if
  * want to add a new hint.
@@ -53,6 +32,7 @@ function toggleHintEditor(objButton){
             '</textarea>',
             '<button type="button" id="saveHint">Save Hint</button>',
             //'<input type="submit" name="saveHint" value="Save Hint"/>',
+            '<button type="button" id="uploadImage">Upload Image</button>',
             '</form>'
         ].join('');
 
@@ -60,6 +40,13 @@ function toggleHintEditor(objButton){
         
         objButton.innerHTML = "Remove Hint";
 
+        /* Probably the proper way to add event listeners. Caters to IE browsers.
+        if (hintSwitch.addEventListener){
+            hintSwitch.addEventListener("click", toggleHintEditor(), false);
+        } else if (hintSwitch.attachEvent) {
+            hintSwitch.attachEvent('onclick', toggleHintEditor());
+        }
+        */
         document.getElementById("saveHint")
             .addEventListener("click", saveHint, false);
     } else {
@@ -77,4 +64,15 @@ function toggleHintEditor(objButton){
  */
 function saveHint(){
     translateToPG(document.getElementById("hintInput").value);
+}
+
+/**
+ * Initiates the streamlined and automated process for inserting an image into 
+ * a problem. 
+ *
+ * TODO Currently will need to request the size of the desired display size of
+ * the image from the user, and will only be able to append the image at the end
+ * of the problem, otherwise a location in the problem will need set via editor.
+ */
+function uploadImage(){
 }
