c6c99202900722be791ebe2f6596e5cd20e082a5
prijatelj <theomnipotentwoof@gmail.com>
2017-03-13 10:46:53 -0400

    last minute bandaid

--- sprint1/HintEditor.js
+++ sprint1/HintEditor.js
@@ -5,10 +5,15 @@
  * @author Derek S. Prijatelj
  */
 
+var mainImgDataArr = [];     // stores image meta data for main problem content
+var hintImgDataArr = []; // stores image meta data for hint content
+
 /**
  * toggles the hint editor. Each hint's individual content may be edited by
  * selecting the proper hint number in the drop down. select add hint if
  * want to add a new hint.
+ *
+ * @param objButton the html document element that called this function
  */
 function toggleHintEditor(objButton){
     /*
@@ -19,12 +24,7 @@ function toggleHintEditor(objButton){
      */
 
     var hintEditor = document.getElementById("hintEditor");
-
-    /*
-     * FIXME? add client side validation. NOTE: this adds no seccurity measures 
-     * Have not included validation, because this string input will be 
-     * translated to PG code and then WeBWorK should validate the input itself.
-     */
+    
     if (hintEditor.innerHTML.replace(/\s/g, '') === ''){
         var addedHTML = [
             '<form id="hintForm" name="hintForm">',
@@ -32,7 +32,7 @@ function toggleHintEditor(objButton){
             '</textarea>',
             '<button type="button" id="saveHint">Save Hint</button>',
             //'<input type="submit" name="saveHint" value="Save Hint"/>',
-            '<button type="button" id="uploadImage">Upload Image</button>',
+            '<button type="button" onclick="uploadImage(true)">Upload Image</button>',
             '</form>'
         ].join('');
 
@@ -67,15 +67,42 @@ function saveHint(){
 }
 
 /**
+ * Contains all meta data of an image for displaying in PG code.
+ *
+ * @param fileName name of the image file
+ * @param width integer display size of image width
+ * @param hieght integer display size of image height
+ */
+function imageMetaData(fileName, width, height){
+    var this.fileName = fileName;
+    var this.width = width;
+    var this.height = height;
+}
+
+/**
  * Initiates the streamlined and automated process for inserting an image into 
  * a problem. 
+ *
+ * @param hintImgDataArr optional parameter (since ES6/ES2015) if true, adds 
+ * uploaded image meta data to the global hintImgDataArr array.
  *
  * TODO Currently will need to request the size of the desired display size of
  * the image from the user, and will only be able to append the image at the end
  * of the problem, otherwise a location in the problem will need set via editor.
  */
-function uploadImage(){
-
+function uploadImage(hint = false){
+    var img = prompt("Please enter URL of externally hosted iage", "");
+    var w = prompt("Enter width of image in pixels for desired display", "");
+    var h = prompt("Enter height of image in plixels for desired display", "");
+    
+    if (img != null && w != null && h != null) {
+        if (hint){
+            hintImgDataArr[hintImgDataArr.length] = imageMetaData(img, w, h);
+        } else {
+            mainImgDataArr[mainImgDataArr.length] = imageMetaData(img, w, h);
+        }
+    }
+    
     // navigate to proper problem set
     
     // call browser function
@@ -83,6 +110,4 @@ function uploadImage(){
     // need to save file name, size, and location to be inserted into PG code.
 
     // call upload function
-
-
 }
